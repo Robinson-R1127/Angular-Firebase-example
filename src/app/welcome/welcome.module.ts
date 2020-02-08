b07cf5c91bf15b739e87b034b6e90e8848eba13d
Robinson-R1127 <rabe.robinson01@gmail.com>
2020-02-08 22:34:32 +0900

    add:welcome

--- src/app/welcome/welcome.module.ts
+++ src/app/welcome/welcome.module.ts
@@ -3,10 +3,12 @@ import { CommonModule } from '@angular/common';
 
 import { WelcomeRoutingModule } from './welcome-routing.module';
 import { WelcomeComponent } from './welcome/welcome.component';
+// welcomeModuleに表示させるボタンをMaterialから引用
 import { MatButtonModule } from '@angular/material/button';
 
 @NgModule({
   declarations: [WelcomeComponent],
+  // importsに上記importと同様に"MatButtonModule"を追加する
   imports: [CommonModule, WelcomeRoutingModule, MatButtonModule]
 })
 export class WelcomeModule {}
