beb612bc5ed77714b4cf26e289ffeebeb2493ca4
Robinson-R1127 <rabe.robinson01@gmail.com>
2020-02-18 14:28:31 +0900

    feat(home): Petコンポーネント追加

--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -3,9 +3,11 @@ import { CommonModule } from '@angular/common';
 
 import { HomeRoutingModule } from './home-routing.module';
 import { HomeComponent } from './home/home.component';
+import { PetComponent } from './pet/pet.component';
+import { MatProgressBarModule } from '@angular/material/progress-bar';
 
 @NgModule({
-  declarations: [HomeComponent],
-  imports: [CommonModule, HomeRoutingModule]
+  declarations: [HomeComponent, PetComponent],
+  imports: [CommonModule, HomeRoutingModule, MatProgressBarModule]
 })
 export class HomeModule {}
