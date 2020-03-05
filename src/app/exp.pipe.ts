5881508a698bc5067b1711acb40c917408b8ecf0
Robinson-R1127 <rabe.robinson01@gmail.com>
2020-03-05 15:55:09 +0900

    feat(exp-pipe): パーセント取得できるよう調整

--- src/app/exp.pipe.ts
+++ src/app/exp.pipe.ts
@@ -1,5 +1,6 @@
 import { Pipe, PipeTransform } from '@angular/core';
 import { Pet } from './interfaces/pet';
+import { ClassGetter } from '@angular/compiler/src/output/output_ast';
 
 const expTable = [
   20, // 1
@@ -17,12 +18,17 @@ const expTable = [
   name: 'exp'
 })
 export class ExpPipe implements PipeTransform {
-  transform(pet: Pet, ...args: any[]): any {
+  transform(pet: Pet, type: 'percent' | 'label'): any {
     const totalExp = pet.exp;
     const level = pet.level;
     const baseExp = expTable[level - 2] || 0;
     const nextExp = expTable[level - 1] - baseExp;
     const exp = totalExp - baseExp;
-    return exp + ' / ' + nextExp;
+
+    if (type === 'percent') {
+      return (exp / nextExp) * 100;
+    } else {
+      return exp + ' / ' + nextExp;
+    }
   }
 }
